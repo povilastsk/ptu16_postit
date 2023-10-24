42d0a7887efb25efc2210ed11d08b7ff3c50f0a7
povilastsk <povilastiskus@gmail.com>
2023-10-24 14:20:08 +0300

    komentarai kazn

--- postit_project/postit_api/serializers.py
+++ postit_project/postit_api/serializers.py
@@ -1,11 +1,31 @@
 from rest_framework import serializers
+
 from . import models
 
+ 
 
 class PostSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='user.username')
-    user_id =serializers.ReadOnlyField(source='user.id')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comment_count = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Post
-        fields = ['id', 'title', 'body', 'username', 'user_id', 'created_at']
+        fields = ['id', 'title', 'body', 'username', 'user_id', 'created_at', 'comments', 'comment_count']
+
+    def get_comment_count(self, obj):
+
+        return models.Comment.objects.filter(post=obj).count()
+
+ 
+class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+    post = serializers.ReadOnlyField(source='post.id')
+
+    class Meta:
+
+        model = models.Comment
+
+        fields = ['id', 'post', 'body', 'username', 'user_id', 'created_at']
--- postit_project/postit_api/views.py
+++ postit_project/postit_api/views.py
@@ -37,3 +37,43 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
             return self.update(self.request, *args, **kwargs)
         else:
             raise ValidationError(_('Can only update your own posts'))
+        
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        return models.Comment.objects.filter(post=post)
+    
+    def perform_create(self, serializer):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'],
+            user=self.request.user,
+        )
+        if post.exists():
+            return self.destroy(self.request, *args, **kwargs)
+        else:
+            raise ValidationError(_('Can only delete your own comments'))
+        
+    def put(self, *args, **kwargs):
+        post = models.Comment.objects.filter(
+            pk=kwargs['pk'],
+            user=self.request.user,
+        )
+        if post.exists():
+            return self.update(self.request, *args, **kwargs)
+        else:
+            raise ValidationError(_('Can only update your own comments'))
